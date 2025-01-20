c11b18af0cb874a81b898073208655e746183b13
iBlue <Ahmedanwar10>
2025-01-21 00:47:38 +0200

    adding FutureListViewItem

--- lib/Features/home/presentation/views/widget/home_view_body.dart
+++ lib/Features/home/presentation/views/widget/home_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:bookly_app/Features/home/presentation/views/widget/custom_appbar.dart';
 import 'package:bookly_app/Features/home/presentation/views/widget/custom_list_view_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -10,8 +11,26 @@ class HomeViewBody extends StatelessWidget {
     return const Column(
       children: [
      CustomAppBarWidget(),
-     CustomListViewItem(),
+     FutureBookListView(),
     ],
+    );
+  }
+}
+class FutureBookListView extends StatelessWidget {
+  const FutureBookListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+     height: MediaQuery.of(context).size.height*0.3,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context,index){
+        return const Padding(
+          padding:  EdgeInsets.symmetric(horizontal:6),
+          child:  CustomListViewItem(),
+        );
+      }),
     );
   }
 }

4ce2335bb2677e58444cf3fabe3776fd77c25194
iBlue <Ahmedanwar10>
2025-01-21 13:09:16 +0200

    best seller list view item part one

--- lib/Features/home/presentation/views/widget/home_view_body.dart
+++ lib/Features/home/presentation/views/widget/home_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:bookly_app/Features/home/presentation/views/widget/custom_appbar.dart';
 import 'package:bookly_app/Features/home/presentation/views/widget/future_list_view.dart';
+import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
@@ -9,15 +10,50 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Padding(
-      padding:  EdgeInsets.symmetric(horizontal: 24.0),
-      child:  Column(
+      padding: EdgeInsets.symmetric(horizontal: 24.0),
+      child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-       CustomAppBarWidget(),
-       FutureListView(),
-       SizedBox(height: 40,),
-       Text("Best Seller",style: Styles.titleMedium,),
-      ],
+          CustomAppBarWidget(),
+          FutureListView(),
+          SizedBox(
+            height: 40,
+          ),
+          Text(
+            "Best Seller",
+            style: Styles.titleMedium,
+          ),
+          BestSellerListViewItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.6 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.black,
+                image: const DecorationImage(
+                    fit: BoxFit.fill, image: AssetImage(AssetsData.test2)),
+              ),
+            ),
+          ),
+          Column(
+            children: [],
+          ),
+        ],
       ),
     );
   }

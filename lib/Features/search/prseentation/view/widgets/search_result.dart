7f9cfd9a9838210b09b8182e4f1c42637d0bae71
iBlue <Ahmedanwar10>
2025-01-24 15:44:13 +0200

    Search result done

--- lib/Features/search/prseentation/view/widgets/search_result.dart
+++ lib/Features/search/prseentation/view/widgets/search_result.dart
@@ -1,14 +1,14 @@
 import 'package:bookly_app/Features/home/presentation/views/widget/best_seller_list_view_item.dart';
 import 'package:flutter/material.dart';
 
-class BestSellerListView extends StatelessWidget {
-  const BestSellerListView({super.key});
+class SearchResultListView extends StatelessWidget {
+  const SearchResultListView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       shrinkWrap: true,
-      physics: const NeverScrollableScrollPhysics(),
+     // physics: const NeverScrollableScrollPhysics(), => I want to get Scroll here
      padding:EdgeInsets.zero,
       itemCount: 10,
       itemBuilder: (context,index){
--- lib/Features/search/prseentation/view/widgets/search_view_body.dart
+++ lib/Features/search/prseentation/view/widgets/search_view_body.dart
@@ -1,4 +1,6 @@
 import 'package:bookly_app/Features/search/prseentation/view/widgets/custom_search_text_field.dart';
+import 'package:bookly_app/Features/search/prseentation/view/widgets/search_result.dart';
+import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 class SearchViewBody extends StatelessWidget {
@@ -9,10 +11,19 @@ class SearchViewBody extends StatelessWidget {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 30.0),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           CustomSearchTextField(),
+          SizedBox(height: 16,),
+          Text(
+                  "Search result",
+                  style: Styles.textStyle18,
+                ),
+          SizedBox(height: 6,),   
+          Expanded(child: SearchResultListView()),
         ],
       ),
     );
   }
 }
+

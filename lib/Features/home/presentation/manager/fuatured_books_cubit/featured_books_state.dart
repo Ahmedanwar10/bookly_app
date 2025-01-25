3ca031538734ab347d7d882588dd87c92cec254b
iBlue <Ahmedanwar10>
2025-01-26 01:26:15 +0200

    create Featured Books Cubit

--- lib/Features/home/presentation/manager/fuatured_books_cubit/featured_books_state.dart
+++ lib/Features/home/presentation/manager/fuatured_books_cubit/featured_books_state.dart
@@ -12,7 +12,7 @@ final class FeaturedBooksLoading extends FeaturedBooksState {}
 final class FeaturedBooksFailure extends FeaturedBooksState {
   final String errorMassage;
 
-  const FeaturedBooksFailure({required this.errorMassage});
+  const FeaturedBooksFailure( this.errorMassage);
 }
 final class FeaturedBooksSuccess extends FeaturedBooksState {
   final List<BookModel>books;

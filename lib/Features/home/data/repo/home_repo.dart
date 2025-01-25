451f7c0ef9fc4b6329435b9d77618625a98dddf8
iBlue <Ahmedanwar10>
2025-01-25 13:24:57 +0200

    implement fetchNewsetBooks

--- lib/Features/home/data/repo/home_repo.dart
+++ lib/Features/home/data/repo/home_repo.dart
@@ -3,6 +3,8 @@ import 'package:bookly_app/core/errors/failure.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<Either< Failure ,List <BookModel>>> fetchBestSellerBooks();
-  Future<Either< Failure ,List <BookModel>>>  fetchFutureBooks();
+  Future<Either<Failure, List<BookModel>>> fetchNewsetBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
+  Future<Either<Failure, List<BookModel>>> fetchSimilarBooks(
+      {required String category});
 }

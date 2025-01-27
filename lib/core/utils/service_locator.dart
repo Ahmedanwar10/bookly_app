375db8daf4ff6e4438cac508cab70b225d574766
iBlue <Ahmedanwar10>
2025-01-28 00:35:24 +0200

    setupGetIt

---
+++ lib/core/utils/service_locator.dart
@@ -0,0 +1,15 @@
+import 'package:bookly_app/Features/home/data/repo/home_repo_implem.dart';
+import 'package:bookly_app/core/utils/api_service.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+
+
+void setupGetIt () {
+  getIt.registerSingleton<ApiService>(ApiService(Dio()));
+  getIt.registerSingleton<HomeRepoImpl>(HomeRepoImpl(
+    getIt.get<ApiService>(),
+  ));
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:bookly_app/Features/home/data/repo/home_repo_implem.dart';
+import 'package:bookly_app/Features/home/presentation/manager/fuatured_books_cubit/featured_books_cubit.dart';
+import 'package:bookly_app/Features/home/presentation/manager/newest_book_cubit/newest_book_cubit.dart';
 import 'package:bookly_app/core/utils/app_router.dart';
+import 'package:bookly_app/core/utils/service_locator.dart';
 import 'package:bookly_app/costants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  setupGetIt();
   runApp(const Bookly());
 }
 
@@ -13,15 +19,31 @@ class Bookly extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter.router,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: kPrimaryColor,
-        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
-        // textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => FeaturedBooksCubit(
+          getIt.get<HomeRepoImpl>(),
+          ),
+        ),
+
+        BlocProvider(
+          create: (context) => NewestBookCubit(
+          getIt.get<HomeRepoImpl>(),
+          ),
+        ),
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRouter.router,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: kPrimaryColor,
+          textTheme:
+              GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+          // textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+        ),
+        //home: const SplashView(),
       ),
-      //home: const SplashView(),
     );
   }
 }

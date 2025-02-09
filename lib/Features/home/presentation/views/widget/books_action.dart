c9ffb173cfec2b66221e81e4d351f8e7e20f9b8d
iBlue <Ahmedanwar10>
2025-02-09 22:33:20 +0200

    Implement URL launching with error handling and add custom snackbar utility

--- lib/Features/home/presentation/views/widget/books_action.dart
+++ lib/Features/home/presentation/views/widget/books_action.dart
@@ -1,4 +1,5 @@
 import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/core/utils/function/launch_url.dart';
 import 'package:bookly_app/core/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -25,14 +26,7 @@ class BooksAction extends StatelessWidget {
           Expanded(
             child: CustomButton(
                 onPressed: () async{
-                  Uri uri = bookModel.volumeInfo.previewLink != null
-                      ? Uri.parse(bookModel.volumeInfo.previewLink!)
-                      : Uri.parse('https://www.google.com/');
-                if (!await canLaunchUrl(uri)) {
-                  throw 'Could not launch $uri';
-                } else {
-                  launchUrl(uri);
-             }
+                  await launchCustomUr(context, bookModel.volumeInfo.previewLink);
               },
               fontSize: 16,
               backgroundColor: const Color(0xffEF8262),
@@ -40,11 +34,19 @@ class BooksAction extends StatelessWidget {
               borderRadius: const BorderRadius.only(
                   topRight: Radius.circular(16),
                   bottomRight: Radius.circular(16)),
-              textButton: 'Preview',
+              textButton: getText(bookModel),
             ),
           ),
         ],
       ),
     );
+  }
+  
+  String getText(BookModel bookModel) {
+    if(!(bookModel.volumeInfo.previewLink == null)){
+      return 'Not Available';
+  }else{
+      return 'Preview';
+    }
   }
 }
